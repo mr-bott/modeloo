3bd68d5558f7bebc4732beb868fbbfe5efdbeadb
murali <muralikrishna8309@gmail.com>
2024-11-06 18:52:16 +0530

    server changes

--- Backend/server.js
+++ Backend/server.js
@@ -230,7 +230,7 @@ const pool = mysql.createPool({
 pool.getConnection()
   .then((connection) => {
     connection.release();
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
